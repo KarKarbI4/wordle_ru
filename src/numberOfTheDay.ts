cf58f2226953c665d0a35d7dbbf7f3e0bd8c8b7c
Victor Tyunyakov <tvs.private@gmail.com>
2022-03-26 12:48:02 +0300

    Update solutions start date

--- src/numberOfTheDay.ts
+++ src/numberOfTheDay.ts
@@ -1,5 +1,5 @@
 import { calcNumberOfTheDay } from "./solutionPicker";
 
-export const startWordDate = new Date(2022, 1, 6); // 6 марта 2022 года
+export const startWordDate = new Date(2022, 2, 25); // 25 марта 2022 года
 export const currentDate = new Date();
 export const numberOfTheDay = calcNumberOfTheDay(startWordDate, currentDate);
